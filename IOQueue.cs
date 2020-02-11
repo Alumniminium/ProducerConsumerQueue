388623374c958ada402559e8c6e23aa3ef2720a2
Trrbl <trrbl@her.st>
2020-02-11 10:30:30 +0100

    Laptop->VscRemote

---
+++ IOQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+
+namespace PCQ
+{
+    public class IOQueue
+    {
+        private static IOQueue _Instance;
+        public static IOQueue Instance => _Instance ?? (_Instance = new IOQueue());
+
+        private ConcurrentQueue<(object output, ConsoleColor color)> _Queue = new ConcurrentQueue<(object, ConsoleColor)>();
+        private Thread _WorkerThread;
+        private volatile bool _KeepRunning = true;
+
+        private AutoResetEvent Block = new AutoResetEvent(false);
+        
+        public IOQueue()
+        {
+            _WorkerThread = new Thread(WorkLoop)
+            {
+                IsBackground = true
+            };
+            _WorkerThread.Start();
+        }
+
+        public void Enqueue((object content, ConsoleColor color) pair)
+        {
+            _Queue.Enqueue(pair);
+            Block.Set();
+        }
+
+        private void WorkLoop()
+        {
+            while (_KeepRunning)
+            {
+                Block.WaitOne();
+
+                while (_Queue.TryDequeue(out var pair))
+                {
+                    var output = pair.output.ToString();
+                    var color = pair.color;
+                    
+                    lock (Console.Out)
+                    {
+                        Console.ForegroundColor = color;
+                        Console.WriteLine(output);
+                        Console.ResetColor();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PCQ
+{
+    public static class Program
+    {
+        public static bool USE_QUEUED_OUTPUT,RedDone,BlueDone,GreenDone,WhiteDone = false;
+
+        public static Thread WriteBlueThread, WriteRedThread, WriteGreenThread,WriteWhiteThread;
+        public static ManualResetEvent Block = new ManualResetEvent(false);
+
+        public static void Main()
+        {
+            SafeConsole.Clear();
+            Block.Reset();
+            RedDone = false;
+            BlueDone = false;
+            WhiteDone = false;
+            GreenDone = false;
+
+            SafeConsole.WriteLine("USE QUEUE? (Y/N)");
+            var input = Console.ReadLine().ToUpperInvariant();
+            switch (input)
+            {
+                case "Y":
+                    USE_QUEUED_OUTPUT = true;
+                    break;
+                case "N":
+                    USE_QUEUED_OUTPUT = false;
+                    break;
+                default:
+                    Main();
+                    break;
+            }
+            
+            WriteBlueThread = new Thread(WriteBlueLoop);
+            WriteRedThread = new Thread(WriteRedLoop);
+            WriteGreenThread = new Thread(WriteGreenLoop);
+            WriteWhiteThread = new Thread(WriteWhiteLoop);
+
+            WriteBlueThread.Start();
+            WriteRedThread.Start();
+            WriteGreenThread.Start();
+            WriteWhiteThread.Start();
+            Block.Set();
+
+
+            while (!RedDone || !BlueDone || !WhiteDone || !GreenDone)
+                Thread.Sleep(1);
+
+            if (USE_QUEUED_OUTPUT)
+                Thread.Sleep(100);
+            
+            SafeConsole.WriteLine("Simulation finished. Press ENTER to restart.");
+            Console.ReadLine();
+            Main();
+        }
+
+        public static void WriteBlueLoop()
+        {
+            Block.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                SafeConsole.WriteLine("BLUE", ConsoleColor.Blue, USE_QUEUED_OUTPUT);
+            }
+            BlueDone = true;
+        }
+
+        public static void WriteRedLoop()
+        {
+            Block.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                SafeConsole.WriteLine("RED", ConsoleColor.Red, USE_QUEUED_OUTPUT);
+            }
+            RedDone = true;
+        }
+        public static void WriteGreenLoop()
+        {
+            Block.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                SafeConsole.WriteLine("GREEN", ConsoleColor.Green, USE_QUEUED_OUTPUT);
+            }
+            GreenDone = true;
+        }
+
+        public static void WriteWhiteLoop()
+        {
+            Block.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                SafeConsole.WriteLine("WHITE", ConsoleColor.White, USE_QUEUED_OUTPUT);
+            }
+            WhiteDone = true;
+        }
+    }
+}
